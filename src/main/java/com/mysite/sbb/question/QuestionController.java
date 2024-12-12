51e74207ef7fddb22ba333d5250ebedfca039e25
junho3168 <junho3168@naver.com>
2024-12-12 22:59:10 +0900

    Specific Page

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,29 +1,31 @@
 package com.mysite.sbb.question;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.ui.Model;
-import java.util.List;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionService;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionService.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
-        @GetMapping(value = "/question/detail/{id}")
-        public String detail(Model model, @PathVariable("id") Integer id) {
-            return "question_detail";
 
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+
     Question findBySubjectAndContent(String subject, String content);
+
     List<Question> findBySubjectLike(String subject);
 }
+//2-11 url 프리픽스 알아두기부터 다시 시작.
