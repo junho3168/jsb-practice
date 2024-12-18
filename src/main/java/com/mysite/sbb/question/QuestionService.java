ed068896b6069f4ac72560c38143482cdc17acf4
junho3168 <junho3168@naver.com>
2024-12-18 16:38:50 +0900

    author

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb.question;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.mysite.sbb.user.SiteUser;
 import org.springframework.data.domain.Sort;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -36,11 +38,12 @@ public class QuestionService {
         }
     }
 
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
     }
 
